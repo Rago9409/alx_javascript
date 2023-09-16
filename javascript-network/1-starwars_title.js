7759139fb56b45f22436718eabd94e90ec055922
Regasa <teshomerego@gmail.com>
2023-09-16 09:14:08 +0300

    Update 1-starwars_title.js

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,6 +1,19 @@
 #!/usr/bin/node
-const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
-});
+import requests
+import sys
+
+def get_movie_title(movie_id):
+    url = f"https://swapi-api.alx-tools.com/api/films/{movie_id}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()["title"]
+    else:
+        return None
+
+if __name__ == "__main__":
+    movie_id = sys.argv[1]
+    title = get_movie_title(movie_id)
+    if title is None:
+        print(f"No movie found with ID {movie_id}")
+    else:
+        print(title)
